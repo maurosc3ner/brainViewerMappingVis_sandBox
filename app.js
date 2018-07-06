f2826b2d8556f56d96242a552176568ed2103082
Esteban Correa <maurosc3ner@gmail.com>
2018-07-06 17:43:38 -0500

    carga de colores desde un json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 
 var container, stats, gui;
-var camera, cameraTarget, scene, renderer,geometry, mesh,controls;
+var camera, cameraTarget, scene, renderer,geometry, mesh,controls, regularGeometry, colorArray;
 
 init();
 
@@ -13,6 +13,13 @@ function init() {
   container = document.getElementById( 'container' );
   document.body.appendChild( container );
   
+  $.getJSON("./js/colorJson.json", function (data) {
+    $.each(data, function (index, value) {
+       //console.log(value[1]);
+       colorArray = value;
+
+        });
+    });
 
   camera = new THREE.PerspectiveCamera(45, container.offsetWidth / container.offsetHeight, 0.01, 10000000);
   camera.position.x = 150;
@@ -94,46 +101,223 @@ function init() {
   var visibleUpdate;
 
   var r=255;
-  var colors2;
+
   // PLY file
   var loader = new THREE.PLYLoader();
   loader.load('/NY/NYmeshWithNormalsASC.ply', function(Geometry) {
+
+    //   var material = new THREE.MeshPhongMaterial({
+
+    //     color: new THREE.Color( 0.4, 0.4, 1.0),
+
+    //     vertexColors: THREE.FaceColors,
+    //     specular: new THREE.Color( 0.1, 0.1, 0.1 ),
+    //     emissive: new THREE.Color( 0, 0, 0 ),
+    //     shininess: 200, 
+    //     transparent : true, 
+    //     //opacity: 1, 
+    //     precision: "highp" ,
+    //     wireframe : false,
+    //     flatShading: false
+    // });
+
       // var material = new THREE.MeshLambertMaterial({
-      //     color: 0xffffff,
-      //     vertexColors: THREE.FaceColors,//geometry
-      //     //flatShading: true
-      //     //vertexColors : THREE.NoColors
-      //     //wireframe: true
-      //   });
+      //   color: new THREE.Color( 0.4, 0.4, 1.0),
+      //   vertexColors: THREE.FaceColors,
+      //   emissive: new THREE.Color( 0, 0, 0 ),
+      //   transparent : true, 
+      //   //opacity: 1, 
+      //   precision: "highp" ,
+      //   wireframe : false,
+      //   flatShading: false
+      // });
 
-      var material = new THREE.MeshPhongMaterial({
-        color: 0xff0000, 
-
-        vertexColors: THREE.FaceColors,
-        specular: 0x555555, 
-        shininess: 300, 
+       var material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color( 1.0, 1.0, 1.0),
+        vertexColors: THREE.VertexColors,
+        emissive: new THREE.Color( 0, 0, 0 ),
         transparent : true, 
-        opacity: 1, 
+        //opacity: 1, 
         precision: "highp" ,
-        wireframe : true,
+        wireframe : false,
         flatShading: false
-    });
-      
+      });
+      regularGeometry = new THREE.Geometry().fromBufferGeometry( Geometry );
       //creo la maya geometria vs material
-      mesh = new THREE.Mesh(Geometry, material);
-      colors2 = new Float32Array(Geometry.attributes.position.count*3);
+      
+      // mesh = new THREE.Mesh(regularGeometry, material);
+      // actualizacion de colores de un geometry
+      // window.console.log(regularGeometry);
+      // var varianza = 1000;
+      // var med = 2500;
+      // // faces are indexed using characters
+      // var faceIndices = [ 'a', 'b', 'c', 'd' ];
+      
+      // var color, f, p, vertexIndex;
+      // for ( var i = 0; i < regularGeometry.faces.length; i ++ ) {
+      //   f  = regularGeometry.faces[ i ];
+      //   var red = 0.0;
+      //   var green = 0.0;
+      //   var blue = 1200*(1/(varianza*Math.sqrt(2*3.141592))) * Math.exp(-0.5*Math.pow((i-med)/varianza, 2.0));
+      //   for ( var j = 0; j < 3; j ++ ) {
+      //     vertexIndex = f[ faceIndices[ j ] ];
+      //     p = regularGeometry.vertices[ vertexIndex ];
 
-      for ( var i = 2; i < Geometry.attributes.position.count*3; i ++ ) {
-        var x = Math.random() * r - r / 2;
-        var y = Math.random() * r - r / 2;
-        var z = Math.random() * r - r / 2;
-        var faceColor = Math.random();
-        colors2[ i * 3 ] = ( x / r ) + 0.5;
-        colors2[ i * 3 + 1 ] = ( y / r ) + 0.5;
-        colors2[ i * 3 + 2 ] = ( z / r ) + 0.5;
+      //     color = new THREE.Color( 0xffffff );
+      //     color.setRGB(red,green,blue);
+      //     f.vertexColors[ j ] = color;
+      //   }
+      // }
+      // regularGeometry.colorsNeedUpdate= true;
+
+      //modo 2: actualizacion de colores de un buffer
+      // mesh = new THREE.Mesh(Geometry, material);
+      // colors2 = new Float32Array(Geometry.attributes.position.count*3);
+      // var varianza = 1000;
+      // var med = 2500;
+      // //window.console.log(Geometry.attributes.position.count);
+      // for ( var i = 0; i < 5000; i ++ ) {
+      //   var x = 0;
+      //   var y = 0;
+      //   var z = 1200*(1/(varianza*Math.sqrt(2*3.141592))) * Math.exp(-0.5*Math.pow((i-med)/varianza, 2.0));
+      //   colors2[ i * 3 ] = ( x / r )  ;
+      //   colors2[ i * 3 + 1 ] = ( y / r ) ;
+      //   colors2[ i * 3 + 2 ] = z;//+.05;
+      //   //window.console.log( z);
+      // }
+
+      // for ( var i = 5000; i < 10016; i ++ ) {
+      //   colors2[ i * 3 ] = 1.0 ;
+      //   colors2[ i * 3 + 1 ] = 1.0;
+      //   colors2[ i * 3 + 2 ] = 1.0;
+      // }
+      // Geometry.addAttribute( "color", new THREE.BufferAttribute( colors2, 3 ) );
+      // Geometry.colorsNeedUpdate= true;
+      
+      //modo 2b: actualizacion de colores de un buffer desde archivo 
+      // Funcional!
+      mesh = new THREE.Mesh(Geometry, material);
+      var colors2;
+      colors2 = new Float32Array(Geometry.attributes.position.count*3);
+      
+      window.console.log(colors2.length);
+      for ( var i = 0; i < colorArray.length; i ++ ) {
+        
+        colors2[ i * 3 ] = colorArray[i].r;
+        colors2[ i * 3 + 1 ] = colorArray[i].g;
+        colors2[ i * 3 + 2 ] = colorArray[i].b;
+        if (i == 8)
+          window.console.log("r:"+colorArray[i].r + " g:"+colorArray[i].g+"b:"+colorArray[i].b);
       }
       Geometry.addAttribute( "color", new THREE.BufferAttribute( colors2, 3 ) );
       Geometry.colorsNeedUpdate= true;
+
+      //modo 3: actualizacion de colores de un geometry
+      // mesh = new THREE.Mesh(regularGeometry, material);
+      // window.console.log(regularGeometry);
+      // var varianza = 1000;
+      // var med = 2500;
+      // // faces are indexed using characters
+      // var faceIndices = [ 'a', 'b', 'c', 'd' ];
+      // var redColor= new THREE.Color( 1.0, 0.0, 0.0);
+      // var greenColor= new THREE.Color( 0.0, 1.0, 0.0);
+      // var blueColor= new THREE.Color( 0.0, 0.0, 1.0);
+
+      // var pa = 1000;
+      // var pb = 5000;
+      // var pc = 9000;
+      // var afz = 1000;
+      // var color, f, p, vertexIndex;
+      // for ( var i = 0; i < regularGeometry.faces.length; i ++ ) {
+      //   if(i>pa-afz && i<pa+afz ){
+      //     f  = regularGeometry.faces[ i ];
+      //     for ( var j = 0; j < 3; j ++ ) {
+      //       vertexIndex = f[ faceIndices[ j ] ];
+      //       p = regularGeometry.vertices[ vertexIndex ];
+      //       f.vertexColors[ j ] = redColor;
+      //     }
+      //   }
+      //   if(i>pb-afz && i<pb+afz ){
+      //     f  = regularGeometry.faces[ i ];
+      //     for ( var j = 0; j < 3; j ++ ) {
+      //       vertexIndex = f[ faceIndices[ j ] ];
+      //       p = regularGeometry.vertices[ vertexIndex ];
+      //       f.vertexColors[ j ] = greenColor;
+      //     }
+      //   }
+
+      //   if(i>pc-afz && i<pc+afz ){
+      //     f  = regularGeometry.faces[ i ];
+      //     for ( var j = 0; j < 3; j ++ ) {
+      //       vertexIndex = f[ faceIndices[ j ] ];
+      //       p = regularGeometry.vertices[ vertexIndex ];
+      //       f.vertexColors[ j ] = blueColor;
+      //     }
+      //   }
+        
+      // }
+      // regularGeometry.colorsNeedUpdate= true;
+
+      /// modo 4: actualizacion de colores de un geometry
+      // mesh = new THREE.Mesh(regularGeometry, material);
+      // window.console.log(regularGeometry);
+      // var varianza = 1000;
+      // var med = 2500;
+      // // faces are indexed using characters
+      // var faceIndices = [ 'a', 'b', 'c', 'd' ];
+      // var redColor= new THREE.Color( 1.0, 0.0, 0.0);
+      // var greenColor= new THREE.Color( 0.0, 1.0, 0.0);
+      // var blueColor= new THREE.Color( 0.0, 0.0, 1.0);
+
+      // var pa = 1000;
+      // var pb = 5000;
+      // var pc = 9000;
+      // var afz = 1000;
+      // var color, f, p, vertexIndex;
+      // for ( var i = 0; i < regularGeometry.faces.length; i ++ ) {
+        
+      //   f  = regularGeometry.faces[ i ];
+      //   for ( var j = 0; j < 3; j ++ ) {
+      //     vertexIndex = f[ faceIndices[ j ] ];
+      //     p = regularGeometry.vertices[ vertexIndex ];
+      //     if (j==0)
+      //       f.vertexColors[ j ] = redColor;
+      //     if (j==1)
+      //       f.vertexColors[ j ] = greenColor;
+      //     if (j==2)
+      //       f.vertexColors[ j ] = blueColor;
+      //   }
+      // }
+      // window.console.log(regularGeometry.faces.length);
+      // regularGeometry.colorsNeedUpdate= true;
+
+      //modo 5: actualizacion desde archivo no funciona!!!
+      // mesh = new THREE.Mesh(regularGeometry, material);
+      // window.console.log(regularGeometry);
+      // // faces are indexed using characters
+      // var faceIndices = [ 'a', 'b', 'c', 'd' ];
+      // var color, f, p, vertexIndex;
+      // // 1. first, assign colors to vertices as desired
+      // for ( var i = 1; i < regularGeometry.vertices.length; i++ ) 
+      // {
+      //   color = new THREE.Color( 0xffffff );
+      //   color.setRGB(colorArray[i].r,colorArray[i].g,colorArray[i].b);
+      //   regularGeometry.colors[i] = color; // use this array for convenience
+      // }
+      // // 2. copy the colors to corresponding positions 
+      // //     in each face's vertexColors array.
+      // for ( var i = 0; i < regularGeometry.faces.length; i++ ) 
+      // {
+      //   face = regularGeometry.faces[ i ];
+      //   numberOfSides = ( face instanceof THREE.Face3 ) ? 3 : 4;
+      //   for( var j = 0; j < 3; j++ ) 
+      //   {
+      //     vertexIndex = face[ faceIndices[ j ] ];
+      //     face.vertexColors[ j ] = regularGeometry.colors[ vertexIndex ];
+      //   }
+      // }
+      // regularGeometry.colorsNeedUpdate= true;
+
       // to LPS space
       var RASToLPS = new THREE.Matrix4();
       RASToLPS.set(-1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
@@ -144,6 +328,7 @@ function init() {
       geometry = Geometry;
 
   });
+
 
 
 
@@ -181,8 +366,10 @@ function init() {
   });
   // Lights
   scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
-  addShadowedLight( 1, 1, 1, 0xffffff, 1.35 );
-  addShadowedLight( 0.5, 1, -1, 0xffaa00, 1 );
+  
+  //addShadowedLight( 1, 1, 1, 0xffffff, 1.35 );
+  
+  addShadowedLight( 0.5, 1, -1,new THREE.Color( 0.5, 0.5, 0.5), 1.35 );
   // renderer
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
@@ -200,6 +387,7 @@ function init() {
 
 function addShadowedLight( x, y, z, color, intensity ) {
   var directionalLight = new THREE.DirectionalLight( color, intensity );
+  // var directionalLight = new THREE.AmbientLight( color,intensity );
   directionalLight.position.set( x, y, z );
   scene.add( directionalLight );
   directionalLight.castShadow = true;
@@ -232,7 +420,7 @@ function render() {
   
 
   // if(geometry){
-    mesh.rotation.z += 0.01;
+    //mesh.rotation.z += 0.01;
   //   geometry.colorsNeedUpdate= true;
   //    window.console.log('Ready to rock!!');
   // }
